d214eabb5fe804dae96d7408076a17f4420bd492
(Unknown)jack <142024753+javad-fallah-official@users.noreply.github.com>
2025-11-20 07:27:35 +0330

    refactor(deps): replace python-telegram-bot with aiogram and cleanup utils
    Remove TextProcessor from utils package and update dependencies to use aiogram instead of python-telegram-bot. Clean up unused dependencies in uv.lock.

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,6 @@ Utils package initialization.
 from .formatters import MessageFormatter
 from .keyboards import KeyboardBuilder
 from .files import FileHandler
-from .text import TextProcessor
 from .cache import AsyncCache
 from .validators import Validator
 
@@ -13,7 +12,6 @@ __all__ = [
     "MessageFormatter",
     "KeyboardBuilder", 
     "FileHandler",
-    "TextProcessor",
     "AsyncCache",
     "Validator"
 ]
